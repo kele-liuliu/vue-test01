69ac15048680e8543e453646e35427cc22901260
kele-liuliu <320917565@qq.com>
2021-05-11 20:24:16 +0800

    更新了aaa

---
+++ src/components/a.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="red">
+    <h2>我是aaaaaaaa</h2>
+    <h2>我是aaaaaaaa</h2>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.red h2 {
+  color: red;
+}
+</style>
