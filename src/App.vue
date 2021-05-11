1d4cb197153d89ed45476cdf588bebc24da0fd49
kele-liuliu <320917565@qq.com>
2021-05-11 20:49:03 +0800

    我刚刚添加了a到app中

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-
+    <A />
     <B />
   </div>
 </template>
 
 <script>
+import A from "./components/a.vue";
 import B from "./components/b";
 export default {
   name: "App",
-  components: { B },
+  components: { B, A },
 };
 </script>
 
